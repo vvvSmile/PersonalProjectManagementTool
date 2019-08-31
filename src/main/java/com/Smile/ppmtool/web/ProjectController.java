4bf038c895ec81d6252e9b5523cf4c5fbb2d8922
vvvSmile <xsx0813@126.com>
2019-08-31 13:48:35 +0800

    处理提交project时候每个字段的validation，不报错，而是提示具体哪个字段有问题并且带有error messsage（状态201，201Created）

--- src/main/java/com/Smile/ppmtool/web/ProjectController.java
+++ src/main/java/com/Smile/ppmtool/web/ProjectController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -27,7 +31,18 @@ public class ProjectController {
 
         if(result.hasErrors()){
             //将Project 换成? 才能返回字符串
-            return new ResponseEntity<String>("Invalid project object",HttpStatus.BAD_REQUEST);
+            //return new ResponseEntity<String>("Invalid project object",HttpStatus.BAD_REQUEST);
+
+            //返回FieldErrors List
+            //return new ResponseEntity<List<FieldError>>(result.getFieldErrors(),HttpStatus.BAD_REQUEST);
+
+            //获取具体的error field with message details eg. {"field":"error message","field":"error message"}
+            Map<String,String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+
         }
 
         Project newProject=projectService.saveOrUpdateProject(project);

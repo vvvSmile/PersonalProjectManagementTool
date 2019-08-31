dbcc9e88cbb03287af2d7efa6f62c70577b63c13
vvvSmile <xsx0813@126.com>
2019-08-31 16:02:00 +0800

    自定义异常处理，处理数据持久化的时的exception，比如ID 重复

--- src/main/java/com/Smile/ppmtool/services/ProjectService.java
+++ src/main/java/com/Smile/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.Smile.ppmtool.services;
 
 import com.Smile.ppmtool.domain.Project;
+import com.Smile.ppmtool.exceptions.ProjectIdException;
 import com.Smile.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,14 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
         //lots of Logic here
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception ex){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+ "' already exists");
+        }
+
     }
 
 }

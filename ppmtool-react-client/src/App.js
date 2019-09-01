bf0e31d7b33ca78175ef5dfb13be108ae8df56cf
vvvSmile <xsx0813@126.com>
2019-09-01 12:06:00 +0800

    first react component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
+      <Dashboard></Dashboard>
+      <Dashboard></Dashboard>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to PPMTool</h1>
